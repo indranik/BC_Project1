3d25523c937451975f5a01f927a8201af177b0f6
Guirlyn Olivar <guirlyn@gmail.com>
2018-04-03 08:52:27 -0700

    remove archived files

--- clean_data- Helen.py
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-print("Hello teammates!")
-=======
-print("hello world")
->>>>>>> 1cc70aa8735e393efacd2c7ec96bad125237abab
--- clean_data_g.py
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-insert new data by guirlyn
-=======
-print("hello world")
->>>>>>> 1cc70aa8735e393efacd2c7ec96bad125237abab
